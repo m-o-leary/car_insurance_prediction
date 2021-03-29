5eb377ff3fef897de04aeb22da8f76f0ed6bc6a3
Martin OLeary <martin.oleary@allianz.com>
2021-03-29 17:28:01 +0100

    WIP: Preprocesser code

--- trainer/trainer_lib/modelling/preprocess.py
+++ trainer/trainer_lib/modelling/preprocess.py
@@ -2,3 +2,31 @@
 # Fill missing
 # Feature engineering
 # Store processed data
+from sklearn.pipeline import Pipeline, FeatureUnion
+    from trainer_lib.transformers import SelectFeaturesTransfomer
+from trainer_lib.transformers import CallDurationTransformer
+from trainer_lib.transformers import TimeOfDayTransformer
+from trainer_lib.transformers import MonthNameTransformer
+from trainer_lib.transformers import EducationTransformer
+from trainer_lib.transformers import DatasetCleanerPipeline
+from trainer_lib.transformers import OutcomeTransformer
+from trainer_lib.transformers import JobTransformer
+from trainer_lib.data.config import ALL_COLUMNS
+
+
+
+
+# Get categorical and numeric column names
+# Feature engineering pipeline
+
+feature_engineering = Pipeline([
+    ("add_time_duration", CallDurationTransformer()),
+    ("add_time_of_day", TimeOfDayTransformer()),
+    ("convert_job", JobTransformer(),
+    ("convert_month", MonthNameTransformer()),
+    ("convert_education", EducationTransformer()),
+    ("convert_outcome", OutcomeTransformer()),
+    ("impute_missing", DatasetCleanerPipeline()),
+    ("column_selection", SelectFeaturesTransfomer(features=ALL_COLUMNS))
+])
+
