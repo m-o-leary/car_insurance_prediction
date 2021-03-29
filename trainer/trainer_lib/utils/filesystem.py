cc2ad2c195c5995cc4127320ed3e6db7cb711c11
Martin OLeary <martin.oleary@allianz.com>
2021-03-29 17:29:19 +0100

    Utils module

---
+++ trainer/trainer_lib/utils/filesystem.py
@@ -0,0 +1,42 @@
+import os
+
+def make_dir(path):
+    """
+    Take a path and create it and any subdirectories
+
+    Example:
+        path = "/tmp/year/month/week/day"
+        make_dir(path)
+
+    :param path: Path to create
+    :return: None
+    """
+    # define the name of the directory to be created
+
+    try:
+        os.makedirs(path)
+    except OSError:
+        print (f"Creation of the directory {path} failed")
+
+def is_dir(path):
+    """
+    Check if the supplied path exists and if so, that it is a directory.
+
+    Example:
+        path = "/tmp/year/month/week/day"
+        is_dir(path)
+
+    :param path: Path to check
+    :return: Boolean
+    """
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return True
+        else:
+            raise ValueError(f"{path} is not a valid path.")
+    else:
+        return False
+
+
+
+
