b93fe3f2e57f00d41862c8b78cbd6eef108ce671
Martin OLeary <martin.oleary@allianz.com>
2021-03-29 17:29:44 +0100

    Adding column info to config +

--- trainer/trainer_lib/data/config.py
+++ trainer/trainer_lib/data/config.py
@@ -2,3 +2,34 @@ DATA_URL="https://storage.googleapis.com/kaggle-data-sets/1411/2534/bundle/archi
 TRAIN_DATA = "carInsurance_train.csv"
 TEST_DATA = "carInsurance_test.csv"
 TARGET_VARIABLE = "CarInsurance"
+# Model columns
+ONE_HOT_CATEGORICAL_COLUMNS = [
+    "Job",
+    "Marital",
+    "Communication",
+    "CallTimeOfDay"
+]
+
+SCALABLE_NUMERIC_COLUMNS = [
+    "Age",
+    "Balance",
+]
+
+NUMERIC_COLUMNS = [
+    "LastContactDay",
+    "LastContactMonth",
+    "NoOfContacts",
+    "DaysPassed",
+    "PrevAttempts",
+    "CallDurationMins",
+    "Education"
+]
+
+BINARY_COLUMNS = [
+    "Default",
+    "HHInsurance",
+    "CarLoan",
+    "Outcome"
+]
+# All model columns
+ALL_COLUMNS = ONE_HOT_CATEGORICAL_COLUMNS + SCALABLE_NUMERIC_COLUMNS + NUMERIC_COLUMNS + BINARY_COLUMNS
