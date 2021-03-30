dd37bfabbd04056417f87ce8c4bf0022fcabd83c
Martin OLeary <martin.oleary@allianz.com>
2021-03-30 11:25:59 +0100

    Change to class with static methods ISO functions

--- trainer/trainer_lib/utils/hashing.py
+++ trainer/trainer_lib/utils/hashing.py
@@ -1,10 +1,32 @@
 import hashlib
 import pandas as pd
-import joblib
 
-def get_hash_from_data(df):
+class Hasher:
     """
-    Generates and returns a hash of the provided dataframe.
+    Class to handle hashing for the project.
     """
-    __hash = hashlib.sha1(pd.util.hash_pandas_object(df).values).hexdigest() 
-    return __hash
+    @staticmethod
+    def get_hash_from_data(df):
+        """
+        Generates and returns a hash of the provided dataframe.
+        """
+        __hash = hashlib.sha1(pd.util.hash_pandas_object(df).values).hexdigest()
+        return __hash
+
+    @staticmethod
+    def get_hash_from_pipeline(pipeline):
+        """
+        Generates and returns a hash of the provided pipeline.
+        """
+        __hash = hashlib.sha1(str(pipeline).encode()).hexdigest()
+        return __hash
+
+    @staticmethod
+    def get_hash_from_object(obj):
+        """
+        Hash any python object which implements the __str__ or __repr__ function.
+        :param obj: The object to return a hash for.
+        :return: str
+        """
+        __hash = hashlib.sha1(str(obj).encode()).hexdigest()
+        return __hash
