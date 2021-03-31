5ac1e15c186d9203185142cfffa859eb9b32e945
Martin OLeary <martin.oleary@allianz.com>
2021-03-31 12:18:54 +0100

    Updating models to match FastAPI schema

--- api/app/rest/database/crud.py
+++ api/app/rest/database/crud.py
@@ -6,23 +6,18 @@ from app.rest.schemas import cold_call
 
 
 def get_model(db: Session, model_id: str):
-    return db.query(models.Model).filter(models.Model.id == model_id).first()
+    return db.query(models.Model).filter(models.Model.estimator_id == model_id).first()
 
 def get_models(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Model).offset(skip).limit(limit).all()
 
 def create_model(db: Session, model: cold_call.Model):
-    db_model = models.Model(
-        id=model.id, 
-        name=model.name,
-        params=json.dumps(model.params),
-        score=model.score,
-        date=model.date
-    )
+    model.params = json.dumps(model.params)
+    db_model = models.Model(**model.dict())
     db.add(db_model)
     db.commit()
     # Only needed if the DB adds data to model e.g. auto - incremented ID
-    # db.refresh(db_model)
+    db.refresh(db_model)
     return db_model
 
 def get_prediction(db: Session, pred_id: str):
@@ -37,5 +32,5 @@ def create_prediction_row(db: Session, pred: cold_call.CallPrediction):
     db.add(db_pred)
     db.commit()
     # Only needed if the DB adds data to model e.g. auto - incremented ID
-    # db.refresh(db_pred)
+    db.refresh(db_pred)
     return db_pred
