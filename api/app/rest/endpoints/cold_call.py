850d6ebd1e84cb9409dc079d554f9e5d109db13e
Martin OLeary <moleary1987@gmail.com>
2021-03-31 16:36:11 +0100

    Implement prediction

--- api/app/rest/endpoints/cold_call.py
+++ api/app/rest/endpoints/cold_call.py
@@ -1,6 +1,7 @@
 import json
 import sqlalchemy
 import pandas as pd
+import numpy as np
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, validator, ValidationError
 from typing import Dict, FrozenSet, List
@@ -67,16 +68,36 @@ async def get_prediction(pred_id: str, db: Session = Depends(get_db)):
     return pred
 
 @router.post("/predictions/{model_id}", response_model=CallPredictionSaved, status_code=201, tags=["Model"])
-async def run_prediction(model_id, pred: Call, db: Session = Depends(get_db), clf = Depends(model_prediction.load_pipeline)):
+async def run_prediction(model_id, call: Call, db: Session = Depends(get_db)):
     """
     Submit cold call details for success prediction.
     The result and the details will be stored in the database.
 
     """
+    try:
+        clf = model_prediction.load_pipeline(model_id)
+        call_df = model_prediction.map_call_line_to_prediction(call)
+        pred__ = clf.predict_proba(call_df)
+        print(call_df)
+        print(clf.predict_proba(call_df))
+        # run pred
+        #  ... 
+        cost_of_calling = 15
+        gain_from_selling = 300
+
+        net_gain = ( pred__[0][1] * gain_from_selling ) - ( pred__[0][0]* cost_of_calling )
 
 
-    # run pred
-    #  ... 
-    pred = CallPrediction(**pred.dict(), prediction=1, prediction_proba = 0.89)
+        explanation = "Calling this customer is likely to result in them purchasing car insurance." \
+            if pred__[0][1] > 0.5\
+                 else "Calling this customer is unlikely to result in a successful sale."
+        explanation += f" The expexcted value of this call is ${np.round(net_gain, 1)}"
+        
+        pred = CallPrediction(**call.dict(), 
+            explanation=explanation,
+            prediction=int(pred__[0][1] > 0.5), 
+            prediction_proba = pred__[0][1])
 
-    return crud.create_prediction_row(db=db, pred=pred)
+        return crud.create_prediction_row(db=db, pred=pred)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail=f"No cached model with id:{model_id} could be found!")
