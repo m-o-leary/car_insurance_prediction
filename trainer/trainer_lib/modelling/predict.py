3fdeea13a91611e3ad7f280093d9c3d6bf42bfb4
Martin OLeary <moleary1987@gmail.com>
2021-04-02 00:08:26 +0100

    Adding class to run prediction against FastAPI

--- trainer/trainer_lib/modelling/predict.py
+++ trainer/trainer_lib/modelling/predict.py
@@ -1,5 +1,50 @@
-# Load test data
-# Load trained model
-# Predict test target using trained model
-# Evaluate results 
-# Store details about the model and version it
+import requests
+import json
+from trainer_lib.modelling.model_config import KEYS_MAP, PREDICTION_URL
+from rich.console import Console
+
+console = Console()
+
+class Predict:
+    """
+    Class to get a prediction from the REST API serving out model.
+    """
+    def __init__(self, rows, model_id, endpoint=PREDICTION_URL):
+        self.rows = rows
+        self.model_id = model_id
+        self.endpoint = endpoint
+
+    def __call__(self):
+        """
+        Call this class to get a prediction.
+        """
+
+        # Saving to DB
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # Add to our DB - the FastAPI container must be running!
+        x = requests.post( 
+            f"{self.endpoint}{self.model_id}", 
+            headers = headers, 
+            data = json.dumps([ DataMapper(row.dropna().to_dict()).map() for i, row in self.rows.iterrows() ]) )
+        
+        # console.print(x.text)
+        return x
+        
+class DataMapper:
+    """
+    Class to map from raw data to API schema.
+
+    Schema is available here: http://localhost:3007/redoc#operation/run_prediction_insurance_model_predictions__model_id__post
+    """
+
+    def __init__(self, old_dict, key_map=KEYS_MAP):
+        self.key_map = key_map
+        self.old_dict = old_dict
+
+    def map(self):
+        return { 
+            self.key_map[old_key] : str(val)
+            for old_key, val in self.old_dict.items() }
