4cb5b38a8e92f737daa4f772faa659a06561936b
Martin OLeary <moleary1987@gmail.com>
2021-04-01 21:44:14 +0100

    Move utility classes out into seperate modules

--- trainer/trainer_lib/modelling/train.py
+++ trainer/trainer_lib/modelling/train.py
@@ -4,12 +4,16 @@ import shap
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
-from datetime import date
+import json
 from rich.console import Console
-from sklearn.pipeline import Pipeline, FeatureUnion
-from trainer_lib.utils.filesystem import persist_pipeline, is_dir, make_dir
-from trainer_lib.utils.hashing import Hasher
-from trainer_lib.utils.notebook_config import MODEL_DIR
+from rich.live import Live
+
+# Utils
+from trainer_lib.utils.output import TrainerConsole
+from trainer_lib.data.saving import ModelSaver
+from trainer_lib.data.data_reconstructor import DataReconstructor
+
+# Setup
 np.random.seed(12345)
 console = Console()
 
@@ -23,23 +27,14 @@ from trainer_lib.transformers import EducationTransformer
 from trainer_lib.transformers import DatasetCleanerPipeline
 from trainer_lib.transformers import OutcomeTransformer
 from trainer_lib.transformers import JobTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from trainer_lib.modelling.evaluate import Evaluation
+from trainer_lib.modelling.explain import Explain
 from trainer_lib.data.config import ALL_COLUMNS, ONE_HOT_CATEGORICAL_COLUMNS, SCALABLE_NUMERIC_COLUMNS
 
-# Classifiers
-from xgboost import XGBClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import KFold, GridSearchCV
-
-# Evaluation
-from sklearn.metrics import plot_roc_curve, confusion_matrix
-from sklearn.model_selection import KFold
-
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import GridSearchCV
 
 class PreProcessor:
     
@@ -88,262 +83,100 @@ class Trainer:
     """
     Trainer class to train multiple models and evaluate.
     """
-    MODEL_POST_URL = "http://api:80/insurance_model/models/"
-    NAMES = [
-        # "Linear SVM",
-        # "Decision Tree", 
-        # "Random Forest", 
-        # "Neural Net", 
-        # "AdaBoost",
-        # "Naive Bayes",
-        "XGB"
-    ]
 
-    CLASSIFIERS = [
-        # SVC(kernel="linear", C=0.025),
-        # DecisionTreeClassifier(max_depth=5),
-        # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-        # MLPClassifier(alpha=1, max_iter=1000),
-        # AdaBoostClassifier(),
-        # GaussianNB(),
-        XGBClassifier(objective='binary:logistic', eval_metric='error')
-    ]
-
-    GRID_SEARCH = {
-        "Linear SVM": {},
-        "Decision Tree": {}, 
-        "Random Forest": {}, 
-        "Neural Net": {}, 
-        "AdaBoost": {},
-        "Naive Bayes": {},
-        "XGB": {
-            'model__max_depth': [2, 3],
-            # 'model__n_estimators': [10, 100, 150]
-        }
-    }
-
-    def __init__(self, X, y, names=NAMES, clfs=CLASSIFIERS, grid=GRID_SEARCH, save_path=MODEL_DIR, api_url=MODEL_POST_URL):
+    def __init__(self, X, y, grid, save_path):
         """
-        Instantiate the Trainer class
+        Instantiate the trainer onject with all required data.
 
-        :param names: Classifier names, defaults to NAMES
-        :type names: list of strings, optional
-        :param clfs: Classifiers to fit to the data, defaults to CLASSIFIERS
-        :type clfs: list of sklearn estimator instances, optional
+        :param X: Features dataframe to fit classifier on.
+        :type X: pandas.DataFrame
+        :param y: Target Variable
+        :type y: pandas.Series / numpy.ndarray  
+        :param grid: A dict of classifier names as keys with
+         a tuple of (classifier, grid search dict) as corresponding values,
+        :type grid: dict
+        :param save_path: Location to save pickled model,
+        :type save_path: str
         """
-        self.hasher = Hasher
-        self.api_url = api_url
         self.X = X
         self.y = y
-        self.names = names
-        self.clfs = clfs
         self.grid = grid
         self.data_pipeline = Pipeline([
             ( 'feature_engineering', PreProcessor().make_pipeline()),
             ( "encoder", Encoder().make_pipeline()),
         ])
-        self.X_hash = self.hasher.get_hash_from_data(self.X)
         self.save_path = save_path
 
-        # Check and create dir for data
-        if is_dir(self.save_path) == False:
-            make_dir(self.save_path)
-
-    def train(self):
+    def train(self, experiment_name):
         """
-        Train all the classifiers associated with the 
+        Train all the classifiers passed as args to this class (or ttbe default list).
 
-        :param X: Feature dataframe.
-        :type X: pandas.DataFrame
-        :param y: Target series
-        :type y: pandas.Series or np.ndarray
-        :return: List of tuples containing the name of each classifier and their score.
-        :rtype: List
+        :param experiment_name: Name for the experiment in the database.
+        :type experiment_name: str
+        :return: dict containing the classifiers and their score.
+        :rtype: [type]
         """
         self.best_score = 0
         self.best_classifier = None
         self.performance={}
-
-        for name, model in zip(self.names, self.clfs):
-
-            # Create model pipeline with xgboost classifier
-            console.rule(name)
-
-            clf = Pipeline([
-                ("pre_processing", self.data_pipeline),
-                ('model', model)
-            ])
-
-            clf, score = self.grid_search(clf, self.GRID_SEARCH[name], self.X, self.y)
-
-            if score > self.best_score:
-                self.best_score = score
-                self.best_classifier = clf
-                self.best_classifier__name = name
-
-            self.performance[name] = score
-
-        return self.performance
-
-    def grid_search(self, clf, param_grid, X, y):
-        console.print("Starting grid search...")
         
+        console.print()
+        console.rule(f"Experiment name: {experiment_name}")
+        
+        with Live(TrainerConsole(self).update(), refresh_per_second=4) as live:
+            for name, __ in self.grid.items():
+                
+                model, grid = __
+
+                # Create model pipeline with xgboost classifier
+                clf = Pipeline([
+                    ("pre_processing", self.data_pipeline),
+                    ('model', model)
+                ])
+
+                best_estimator, score, params = self.grid_search(clf, grid, self.X, self.y)
+                # console.print(best)
+                if score > self.best_score:
+                    self.best_score = score
+                    self.best_classifier = best_estimator
+                    self.best_classifier__name = name
+                    self.best_paramas = params
+
+                hash_id = ModelSaver(self.X, self.y, self.save_path).save_best(f"{experiment_name}__{name}", best_estimator, params)
+                self.performance[name] = (score, params, hash_id)
+                live.update(TrainerConsole(self).update(name))
+
+    def grid_search(self, clf, param_grid, X, y, k=10, verbose=0, scoring='roc_auc'):
+        """
+        Perform grid search on classifier arcoss paramater grid
+
+        :param clf: sklearn compatible classifier
+        :type clf: sklearn compatible classifier
+        :param param_grid: Parameter grid to run over.
+        :type param_grid: dic
+        :param X: Features
+        :type X: pandas.Dataframe
+        :param y: target variable
+        :type y: pandas.Series / numpy.ndarray
+        :return: Tuple containing: best estimator, average score over k-folds, best parameters
+        :rtype: Tuple
+        """
         grid = GridSearchCV(
             clf, 
             param_grid, 
-            cv=10, 
+            cv=k, 
             n_jobs=-1, 
-            verbose=5,
-            scoring='roc_auc')
+            verbose=verbose,
+            scoring=scoring)
 
+        # Fit 
         grid.fit(X, y)   
 
         mean_score = grid.cv_results_["mean_test_score"][grid.best_index_]
         std_score = grid.cv_results_["std_test_score"][grid.best_index_]
 
         grid.best_params_, mean_score, std_score
-        self.best_paramas = grid.best_params_
-        console.log(f"Best parameters: {grid.best_params_}")
-        console.log(f"Mean CV score: [green]{mean_score: .6f}[/green]")
-        console.log(f"Standard deviation of CV score: {std_score: .6f}")
-        return grid.best_estimator_, mean_score
 
-    def save_best(self, name):
-        """
-        Save a copy of the pipeline.
+        return (grid.best_estimator_, mean_score, grid.best_params_)
 
-        Get a hash for the raw data, the hashed data and pipeline.
-
-        :param name: Name for model tracking
-        :type name: str
-        :return: dict of saved responses.
-        :rtype: dict
-        """
-        metrics = Evaluation(self.best_classifier, self.X, self.y).run()
-        explainer = Explain(self.best_classifier, self.X)
-        hash_dict = {
-            "pipeline_hash": self.hasher.get_hash_from_pipeline(self.best_classifier),
-            "raw_hash": self.X_hash,
-            "processed_hash": self.hasher.get_hash_from_data(explainer.X_df),
-        }
-
-        hash_dict.update({'estimator_id': self.hasher.get_hash_from_object(hash_dict)})
-        model_id = f"{self.save_path}{hash_dict['estimator_id']}.pkl"
-        persist_pipeline(self.best_classifier, model_id)
-        console.print(f"Model saved as {model_id}!")
-
-        # Saving to DB
-        console.print(self.api_url)
-        self.save_object_ = {
-            "estimator_id": hash_dict['estimator_id'],
-            "name": name,
-            "params": self.best_paramas,
-            "f1_score": metrics['f1_score'],
-            "accuracy": metrics['accuracy'],
-            "d": date.today(),
-            "raw_hash": hash_dict['raw_hash'],
-            "processed_hash": hash_dict['processed_hash'],
-            "pipeline_hash": hash_dict['pipeline_hash']
-        }
-        # Add to our DB - the FastAPI container must be running!
-        myobj = {'somekey': 'somevalue'}
-
-        x = requests.post( self.api_url, data = self.save_object_ )
-
-        if x.ok:
-            return 
-
-        return hash_dict
-
-class Evaluation:
-    """
-    Evaluate a fitted classifier.
-    """
-
-    def __init__(self, clf, X, y):
-        self.clf = clf
-        self.X = X
-        self.y = y
-
-    def get_plot_roc(self):
-        """
-        Get plot of Receiver operator curve.
-        """
-        clf, X, y = self.clf, self.X, self.y
-        pl, ax = plt.subplots(figsize=(11,5))
-        p = plot_roc_curve(clf, X, y, ax=ax)
-        clf_name = list(clf.named_steps.keys())[-1]
-        plt.title(f"Receiver Operator Curve for {clf_name} Classifier")
-        plt.show()
-        
-    def get_confusion_matrix(self):
-        """
-        Calculate the confusion matrix and return
-        """
-        clf, X, y = self.clf, self.X, self.y
-        preds = clf.predict(X)
-        return confusion_matrix(y, preds)
-
-    def get_f1_score(self, confusion_matrix):
-        """
-        Calculate the f1 score.
-        """
-        TP = confusion_matrix[0,0]
-        TN = confusion_matrix[1,1]
-        FP = confusion_matrix[0,1]
-        FN = confusion_matrix[1,0]
-        return TP / (TP + 0.5 * (FP + FN))
-
-    def get_accuracy(self, confusion_matrix):
-        """
-        Calculate the accuracy
-         (correct predictions as a proportion of all predictions).
-        """
-        TP = confusion_matrix[0,0]
-        TN = confusion_matrix[1,1]
-        FP = confusion_matrix[0,1]
-        FN = confusion_matrix[1,0]
-        return (TP + TN) / ( TP + TN + FP + FN) 
-
-    def run(self, plot=False):
-        """
-        Take a classifier and test data and evaluate the performance.
-
-        Returns a dict with evaluation metrics.
-        """
-        confusion_ = self.get_confusion_matrix()
-        if plot:
-            self.get_plot_roc()
-        return {
-            "f1_score": self.get_f1_score(confusion_),
-            "accuracy": self.get_accuracy(confusion_)
-        }
-
-class Explain:
-    """
-    Model explainability class.
-    """
-
-    def __init__(self, clf, X ):
-
-        self.clf = clf
-        self.X = X
-        self.explainer = shap.Explainer(clf.steps[-1][1])
-        self.make_dataframe()
-        self.shap_values = self.explainer(self.X_df)
-
-    def make_dataframe(self):
-        names = list(
-            self.clf['pre_processing']['encoder']
-            .transformers_[0][1]['cat']
-            .get_feature_names(ONE_HOT_CATEGORICAL_COLUMNS))
-            
-        names.extend([c for c in ALL_COLUMNS if c not in ONE_HOT_CATEGORICAL_COLUMNS])
-        self.X_df = pd.DataFrame(self.clf.named_steps['pre_processing'].transform(self.X), columns=names)
-
-    def waterfall(self, index=0):
-        shap.plots.waterfall(self.shap_values[index], max_display=15)
-
-    def prediction(self, index=0):
-        p = shap.plots.force(self.shap_values[index])
-        p.matplotlib((30,8),True, 35)
+   
